30428f5cf963f4230434b710ae950735ffe9b999
genanik <genanik@hotmail.com>
2020-07-28 19:59:40 +0800

    改进复读中的processText以支持emoji等特殊字符

--- src/main/kotlin/io/genanik/miraiPlugin/Function.kt
+++ src/main/kotlin/io/genanik/miraiPlugin/Function.kt
@@ -3,18 +3,19 @@ package io.genanik.miraiPlugin
 import io.genanik.miraiPlugin.utils.isEqual
 import io.genanik.miraiPlugin.utils.mirrorImage
 import io.genanik.miraiPlugin.utils.removeMsgSource
+import io.genanik.miraiPlugin.utils.translate.Method
 import net.mamoe.mirai.contact.Contact
 import net.mamoe.mirai.message.GroupMessageEvent
 import net.mamoe.mirai.message.data.*
-import io.genanik.miraiPlugin.utils.translate.Method
 import java.text.SimpleDateFormat
 import java.util.*
+
 
 /**
  * 判断出现两条相同内容后 将内容镜像并返回镜像的MessageChain
  * 构造时需要传入GroupMessageEvent
  */
-class MessagesRepeatFunction (message: GroupMessageEvent) {
+class MessagesRepeatFunction(message: GroupMessageEvent) {
 
     private var lastMessage: GroupMessageEvent = message // 不保证没有MessageSource块
     private var times = 1
@@ -85,26 +86,68 @@ class MessagesRepeatFunction (message: GroupMessageEvent) {
         if (text == null){
             return
         }
-        var tmp = ""
+        val src = text.toString()
         val symbolRaw = arrayOf('[', ']', '(', ')', '（', '）', '{', '}', '【', '】', '「', '」', '“', '”', '/', '\\', '‘', '’', '<', '>', '《', '》')
         val symbolNew = arrayOf(']', '[', ')', '(', '）', '（', '}', '{', '】', '【', '」', '「', '”', '“', '\\', '/', '’', '‘', '>', '<', '》', '《')
-        text.toString().forEach {
-            // 字符替换
-            val symbolInt = symbolRaw.findOrNull(it)
 
-            tmp = when {
-                symbolInt != null -> {
-                    "" + symbolNew[symbolInt] + tmp
-                }
-                else -> {
-                    it + tmp
+        //1.得到代码点数量，也即是实际字符数，注意和length()的区别
+        //举例：
+        //一个emoji表情是一个字符，codePointCount()是1，length()是2。
+        val cpCount = src.codePointCount(0, src.length)
+
+        //2.得到字符串的第一个代码点index，和最后一个代码点index
+        //举例：比如3个emoji表情，那么它的cpCount=3；firCodeIndex=0；lstCodeIndex=4
+        //因为每个emoji表情length()是2，所以第一个是0-1，第二个是2-3，第三个是4-5
+        val firCodeIndex = src.offsetByCodePoints(0, 0)
+        val lstCodeIndex = src.offsetByCodePoints(0, cpCount - 1)
+        var result = ""
+        var index = firCodeIndex
+        while (index <= lstCodeIndex) {
+            //3.获得代码点，判断是否是emoji表情
+            //注意，codePointAt(int) 这个int对应的是codeIndex
+            //举例:3个emoji表情，取第3个emoji表情，index应该是4
+            val codepoint = src.codePointAt(index)
+            result = if (isEmojiCharacter(codepoint)) {
+                // 特殊字符
+                val length = if (Character.isSupplementaryCodePoint(codepoint)) 2 else 1
+                src.substring(index, index + length) + result
+            } else {
+                // 普通字符
+                val rawChar = codepoint.toChar()
+                val symbolInt = symbolRaw.findOrNull(rawChar)
+                if (symbolInt != null) {
+                    symbolNew[symbolInt] + result
+                }else{
+                    rawChar + result
                 }
             }
+            //4.确定指定字符（Unicode代码点）是否在增补字符范围内。
+            //因为除了表情，还有些特殊字符也是在增补字符方位内的。
+            index += if (Character.isSupplementaryCodePoint(codepoint)) 2 else 1
         }
-        this.add(tmp)
+        this.add(result)
+        
         hasBeenProcessed = true
     }
-
+    
+    private fun isEmojiCharacter(codePoint: Int): Boolean {
+        return (codePoint in 0x2600..0x27BF // 杂项符号与符号字体
+                || codePoint == 0x303D || codePoint == 0x2049 || codePoint == 0x203C || codePoint in 0x2000..0x200F //
+                || codePoint in 0x2028..0x202F //
+                || codePoint == 0x205F //
+                || codePoint in 0x2065..0x206F //
+                /* 标点符号占用区域 */
+                || codePoint in 0x2100..0x214F // 字母符号
+                || codePoint in 0x2300..0x23FF // 各种技术符号
+                || codePoint in 0x2B00..0x2BFF // 箭头A
+                || codePoint in 0x2900..0x297F // 箭头B
+                || codePoint in 0x3200..0x32FF // 中文符号
+                || codePoint in 0xD800..0xDFFF // 高低位替代符保留区域
+                || codePoint in 0xE000..0xF8FF // 私有保留区域
+                || codePoint in 0xFE00..0xFE0F // 变异选择器
+                || codePoint >= 0x10000) // Plane在第二平面以上的，char都不可以存，全部都转
+    }
+    
     private fun <Char> Array<Char>.findOrNull(targetChar: Char): Int? {
         for ((index, i) in this.withIndex()){
             if (i == targetChar){
@@ -114,7 +157,6 @@ class MessagesRepeatFunction (message: GroupMessageEvent) {
         }
         return null
     }
-
 }
 
 /**
@@ -175,7 +217,7 @@ class DonaldTrumpFunction {
         taowa.add("禁止")
     }
 
-    private fun textStruct(singleWord:String): String{
+    private fun textStruct(singleWord: String): String{
         sentence.add("我们有全球最好的${singleWord}专家\n——特朗普")
         sentence.add("对于${singleWord}，没什么需要恐慌的\n——特朗普")
         sentence.add("有些人会说我非常 非常非常有才，特别是${singleWord}方面\n——特朗普")
