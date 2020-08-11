9445f3a0580d9d6b20f3230ec5c95b8381a24ad6
genanik <genanik@hotmail.com>
2020-08-11 08:11:18 +0800

    更改/help内容

--- src/main/kotlin/io/genanik/daHuo/abelCommand/UserCommand.kt
+++ src/main/kotlin/io/genanik/daHuo/abelCommand/UserCommand.kt
@@ -7,11 +7,11 @@ import net.mamoe.mirai.message.data.MessageChainBuilder
 fun AbelPlugins.regHelp(){
     this.regCommand("/help", "展示帮助信息") {
         val result = MessageChainBuilder()
-        result.add("你好你好\n\n")
+        result.add("你好你好\n")
         for (i in this.getAllCommands()) {
             result.add("* $i  ${this.getCommandDescription()[i]}\n")
         }
-        result.add("\n咱介绍完指令了，然后。。然后。。。。\n该介绍功能了\n\n")
+        result.add("\n咱介绍完指令了\n该介绍功能了\n\n")
         for (i in this.getAllFunctions()) {
             result.add("* $i  ${this.getFunctionDescription()[i]}\n")
         }
@@ -19,7 +19,9 @@ fun AbelPlugins.regHelp(){
             "\n其他功能：\n" +
                     "* \"功能名称+打开了嘛\" 获取功能运行状态\n"
         )
-        result.add("* /adminHelp 获取管理员帮助信息")
+        result.add("* /adminHelp 获取管理员帮助信息\n")
+        result.add("\n指令触发的话只需要发一条与指令一模一样的消息就可以了\n")
+        result.add("功能触发条件..emm写在功能描述里了")
         return@regCommand result.asMessageChain()
     }
 }
