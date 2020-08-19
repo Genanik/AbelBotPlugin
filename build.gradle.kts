8af61b8a37d0c13c667e7160bccd2f3260c36381
genanik <genanik@hotmail.com>
2020-08-19 19:05:47 +0800

    更新至3.0.0 RC3

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,7 +14,7 @@ repositories {
 }
 
 group = "io.genanik"
-version = "3.0.0-RC2"
+version = "3.0.0-RC3"
 
 val miraiCoreVersion = "1.0.4"
 val miraiConsoleVersion = "0.5.2"
--- src/main/kotlin/Versions.kt
+++ src/main/kotlin/Versions.kt
@@ -1,7 +1,7 @@
 object Versions {
 
     object DaHuo {
-        const val version = "3.0.0-RC2"
+        const val version = "3.0.0-RC3"
     }
 
     object AbelFramework {
